03c5f241917f63b6dbec89291dc37075d2453795
Debojyoti <debojyoti.das@umu.se>
2021-06-18 16:31:23 +0200

    modifications to sra.py

--- cellbuster_geo.py
+++ cellbuster_geo.py
@@ -1,19 +1,27 @@
-import requests
-import sys
-import pandas as pd
-from io import StringIO
-from tqdm import tqdm
-import urllib
-from pysradb import cli
-import util
+import os, sys
 import GEOparse
+import pysradb
+
+from pysradb.sraweb import SRAweb
+from pysradb.geodb import GEOdb
+from pysradb.search import GeoSearch
 
 
 def cellbuster_geo():
-    gse = GEOparse.get_GEO(geo="GSE174786", destdir="./")
+#    gse = GEOparse.get_GEO(geo="GSE158984", destdir="./")
+#    gse = GEOparse.get_GEO(geo="GSE1563", destdir="./")
 
-    gse.download_SRA("debojyoti.das@umu.se")
-    gse.download_supplementary_files()
+    db = GeoSearch(accession = "GSE57249")
+    import pdb;pdb.set_trace()
+    df_metadata = db.geo_metadata('GSE57249', detailed=True)
+    list=df_metadata["run_accession"].tolist()
+    
+#    gse.download_SRA("debojyoti.das@umu.se", nproc=16)
+#    gse.download_supplementary_files()
+    for i in list:
+        print(i)
+
+    print(" Download complete!!")
 
 
 if __name__ == "__main__":
