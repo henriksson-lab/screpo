fb20f7821f079573377ff97233cd8648842250b3
Debojyoti <debojyoti.das@umu.se>
2021-06-23 14:32:45 +0200

    renaming_sra modified

--- renaming_sra.py
+++ renaming_sra.py
@@ -4,40 +4,37 @@ from Bio import SeqIO
 import sys, os, re, gzip
 
 
-def renaming_sra(file1,file2):
-    fastq1 = gzip.open(file1, "rt")
-    fastq2 = gzip.open(file2, "rt")
+def renaming_sra(files):
 
-    records1 = SeqIO.parse(fastq1, "fastq")
-    records2 = SeqIO.parse(fastq2, "fastq")
+    for file in files:
+        fastq = gzip.open(file, "rt")
 
-    for record in records1:
-        name1 = str(record.name)
-        description1 = str(record.description)
-        break
+        records = SeqIO.parse(fastq, "fastq")
 
-    for record in records2:
-        name2 = str(record.name)
-        description2 = str(record.description)
-        break
+        for record in records:
+            name = str(record.name)
+            description = str(record.description)
+            break
 
+        fastq.close()
 
-    read_file1 = description1.replace(name1,"").split(":")[0]
-    read_file2 = description2.replace(name2,"").split(":")[0]
+        read_file = description.replace(name,"").split(":")[0]
+        read_file = read_file.replace(" ","")
 
-    print("read_file1")
-    print(read_file1)
-    print("read_file2")
-    print(read_file2)
+        print(read_file)
 
-    if read_file1 == "1":
-        os.rename(file1,(file1.split("_")[0]).split(".")[0]+"_R1.fastq.gz")
-        os.rename(file2,(file2.split("_")[0]).split(".")[0]+"_R2.fastq.gz")
-    if read_file1 == "2":
-        os.rename(file2,(file2.split("_")[0]).split(".")[0]+"_R1.fastq.gz")
-        os.rename(file1,(file1.split("_")[0]).split(".")[0]+"_R2.fastq.gz")
-        
-    # import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
+
+        if read_file == "1":
+
+            os.rename(file,(file.split("_")[0]).split(".")[0]+"_R1.fastq.gz")
+            print("Read => " + read_file)
+        elif read_file == "3":
+            os.rename(file,(file.split("_")[0]).split(".")[0]+"_R2.fastq.gz")
+            print("Read => " + read_file)
+        else:
+            os.rename(file,(file.split("_")[0]).split(".")[0]+"_I1.fastq.gz")
+            print("Read => " + read_file)
 
 
 
@@ -45,6 +42,9 @@ def renaming_sra(file1,file2):
 
 
 if __name__ == '__main__':
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
-    renaming_sra(file1,file2)
+    files = sys.argv[1:]
+    print("Files read\n")
+    for file in files:
+        print(file)
+
+    renaming_sra(files)
