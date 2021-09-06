7696f273f2d907417d097e4d971323a474fa5924
Debojyoti <debojyoti.das@umu.se>
2021-09-06 13:26:00 +0200

    handles fastq file both zipped and unzipped

--- fastqfileinfo.py
+++ fastqfileinfo.py
@@ -1,11 +1,16 @@
 '''
     This function is able to retrive info about the fastq file
 '''
-import sys, gzip
+import sys, re, gzip
 from Bio import SeqIO
 
 def fastqinfo(file):
-    with gzip.open(file, "rt") as handle:
+    if re.search(".gz",file):
+        container = gzip.open(file, "rt")
+    else:
+        container = open(file,"rt")
+
+    with container as handle:
         for record in SeqIO.parse(handle, "fastq"):
             return(record)
             break
